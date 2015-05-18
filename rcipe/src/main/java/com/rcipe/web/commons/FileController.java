c849dae8c576b694661c09659739c4b3c2e5195d
ImJaeYoung <dlawowo01@gmail.com>
2015-05-18 21:17:59 +0900

    .

--- rcipe/src/main/java/com/rcipe/web/commons/FileController.java
+++ rcipe/src/main/java/com/rcipe/web/commons/FileController.java
@@ -10,21 +10,13 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.rcipe.commons.FileUtil;
 import com.rcipe.service.commons.FileService;
 import com.rcipe.service.domain.User;
-import com.rcipe.service.user.UserService;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 
 /*
  * 파일 업로드Test class 일단 file upload위치는 C:\\fileUploadTest폴더 밑으로 했고 일단 확장자가 JPG만 할수 있게 해놨다.
