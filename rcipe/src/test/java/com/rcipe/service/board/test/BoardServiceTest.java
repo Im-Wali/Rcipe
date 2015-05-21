af9d9f2183aa45ac5712d09faf57868195542e34
leejieun <gkf311@gmail.com>
2015-05-21 20:09:48 +0900

    boardServiceTEst

--- rcipe/src/test/java/com/rcipe/service/board/test/BoardServiceTest.java
+++ rcipe/src/test/java/com/rcipe/service/board/test/BoardServiceTest.java
@@ -1,9 +1,36 @@
 package com.rcipe.service.board.test;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.rcipe.commons.Search;
+import com.rcipe.service.board.BoardService;
+import com.rcipe.service.domain.Board;
+
+
 public class BoardServiceTest {
 
+	@Value("#{commonProperties['pageUnit']}")
+	int pageUnit;
+
+	@Value("#{commonProperties['pageSize']}")
+	int pageSize;
+	
 	public BoardServiceTest() {
 		// TODO Auto-generated constructor stub
+
+	}
+	static public void main(String args[])throws Exception{
+		ApplicationContext context =
+				new ClassPathXmlApplicationContext(
+						new String[] {	"/config/*.xml"	 }
+						);
+		BoardService boardService = (BoardService) context.getBean("boardServiceImpl");
+		Board board=new Board(1,"user01","title77","contents",0,7777);
+		Search search = new Search(1,"1"," ",1);
+		
+		System.out.println("getBoardList Test :"+boardService.getBoardList(search));
 	}
 
 }
