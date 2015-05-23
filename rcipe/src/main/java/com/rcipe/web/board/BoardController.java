b1bedad59633324a13e807adb7af5cf2fcb7fe36
ImJaeYoung <dlawowo01@gmail.com>
2015-05-23 14:32:48 +0900

    .

--- rcipe/src/main/java/com/rcipe/web/board/BoardController.java
+++ rcipe/src/main/java/com/rcipe/web/board/BoardController.java
@@ -1,8 +1,9 @@
 package com.rcipe.web.board;
 
-import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +14,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.rcipe.commons.Page;
 import com.rcipe.commons.Search;
 import com.rcipe.service.board.BoardService;
 import com.rcipe.service.domain.Board;
+import com.rcipe.service.domain.User;
 
 @Controller
 @RequestMapping("/board")
@@ -37,12 +40,41 @@ public class BoardController {
 	@Value("#{commonProperties['pageSize']}")
 	int pageSize;
 	
+	
 	@RequestMapping(value = "/inserBoard", method = RequestMethod.POST)
-	public  void  uploadBoard(Model model,@ModelAttribute("board") Board board,
+	public  String  inserBoard(Model model,@ModelAttribute("board") Board board,
 			HttpSession session) throws Exception {
-		System.out.println(board);
+		User user=new User("user01","user01@naver.com","1111","!!!!");
+//		User user=(User)session.getAttribute("user");
+		board.setBoardImgPath((String)session.getAttribute("boardImgPath"));
+		board.setNickname(user.getNickname());
+		boardService.insertBoard(board);
+		session.removeAttribute("boardImgPath");
+		return "forward:/main/boardList.jsp";
 	}
-
+	
+	@RequestMapping(value = "/viewModifyBoard", method = RequestMethod.GET)
+	public  String  viewModifyBoard(Model model,@RequestParam("boardNo") String boardNo,HttpSession session)throws Exception{
+		Board board=boardService.getBoard(Integer.parseInt(boardNo));
+		session.setAttribute("boardImgPath",board.getBoardImgPath());
+		model.addAttribute("board",board);
+		return "forward:/main/modifyBoard.jsp";
+	}
+	
+	@RequestMapping(value = "/modifyBoard", method = RequestMethod.POST)
+	public  String  modifyBoard(Model model,@ModelAttribute("board") Board board,HttpSession session)throws Exception{
+		boardService.updateBoard(board);
+		session.removeAttribute("boardImgPath");
+		model.addAttribute("board",boardService.getBoard(board.getBoardNo()));
+		return "forward:/main/viewBoard.jsp";
+	}
+	
+	@RequestMapping(value = "/viewBoard", method = RequestMethod.GET)
+	public  String  viewBoard(Model model,@RequestParam("boardNo") String boardNo)throws Exception{
+		model.addAttribute("board",boardService.getBoard(Integer.parseInt(boardNo)));
+		return "forward:/main/viewBoard.jsp";
+	}
+	
 	@RequestMapping(value = "/getBoardList", method = RequestMethod.GET)
 	public ModelAndView  getBoardList( @ModelAttribute("search") Search search) throws Exception {
 		System.out.println("getBoardList start");
@@ -65,5 +97,5 @@ public class BoardController {
 		
 		return modelAndView;
 	}
-	
+
 }
