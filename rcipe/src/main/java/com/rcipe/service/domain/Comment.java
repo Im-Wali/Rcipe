4cbb92822101d87962fe0b82bf2d5f9bd954b7d1
ImJaeYoung <dlawowo01@gmail.com>
2015-05-24 17:59:45 +0900

    .

--- rcipe/src/main/java/com/rcipe/service/domain/Comment.java
+++ rcipe/src/main/java/com/rcipe/service/domain/Comment.java
@@ -5,68 +5,104 @@ import java.util.Date;
 public class Comment {
 
 	/*
-	 	commentNo		Integer
+	 	commentNo			Integer
 		commentReNo		Integer
-		nicname			String
-		commentContents	String
+		nicname					String
+		commentContent	String
 		commentDate		Date
+		userImg			 		String
+		contentNo 				 Integer 
 	 */
 	
 	private Integer commentNo;
 	private Integer commentReNo;
 	private String nickname;
-	private String commentContents;
+	private String commentContent;
 	private Date commentDate;
+	private String userImg;
+	private Integer contentNo;
 	
+
 	public Comment() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	
 	public Integer getCommentNo() {
 		return commentNo;
 	}
 
-	public void setCommentNo(Integer commentNo) {
-		this.commentNo = commentNo;
-	}
 
 	public Integer getCommentReNo() {
 		return commentReNo;
 	}
 
-	public void setCommentReNo(Integer commentReNo) {
-		this.commentReNo = commentReNo;
-	}
 
 	public String getNickname() {
 		return nickname;
 	}
 
-	public void setNicname(String nicname) {
-		this.nickname = nicname;
+
+	public String getCommentContent() {
+		return commentContent;
 	}
 
-	public String getCommentContents() {
-		return commentContents;
-	}
-
-	public void setCommentContents(String commentContents) {
-		this.commentContents = commentContents;
-	}
 
 	public Date getCommentDate() {
 		return commentDate;
 	}
 
+
+	public String getUserImg() {
+		return userImg;
+	}
+
+
+	public Integer getContentNo() {
+		return contentNo;
+	}
+
+
+	public void setCommentNo(Integer commentNo) {
+		this.commentNo = commentNo;
+	}
+
+
+	public void setCommentReNo(Integer commentReNo) {
+		this.commentReNo = commentReNo;
+	}
+
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+
+	public void setCommentContent(String commentContent) {
+		this.commentContent = commentContent;
+	}
+
+
 	public void setCommentDate(Date commentDate) {
 		this.commentDate = commentDate;
 	}
 
+
+	public void setUserImg(String userImg) {
+		this.userImg = userImg;
+	}
+
+
+	public void setContentNo(Integer contentNo) {
+		this.contentNo = contentNo;
+	}
+
+
 	@Override
 	public String toString() {
 		return "Comment [commentNo=" + commentNo + ", commentReNo="
-				+ commentReNo + ", nickname=" + nickname + ", commentContents="
-				+ commentContents + ", commentDate=" + commentDate
+				+ commentReNo + ", nickname=" + nickname + ", commentContent="
+				+ commentContent + ", commentDate=" + commentDate
 				+ ", toString()=" + super.toString() + "]";
 	}
 
