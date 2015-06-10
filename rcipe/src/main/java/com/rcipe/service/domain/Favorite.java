8c33c997edbd5d6097cb89c6e5601c07a35e7390
konjou <konjou09@gmail.com>
2015-06-10 18:23:39 +0900

    .

--- rcipe/src/main/java/com/rcipe/service/domain/Favorite.java
+++ rcipe/src/main/java/com/rcipe/service/domain/Favorite.java
@@ -10,43 +10,63 @@ public class Favorite {
 		favoriteDate		Date
 	 */
 	
-	private Recipe recipe;
-	private String favoriteTitle;
-	private Date favoriteDate;
+	private Integer recipeNo;
+	private String nickname;
+	private String favorTitle;
+	private Date favorDate;
 
 	public Favorite() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Recipe getRecipe() {
-		return recipe;
+	public Favorite(Integer recipeNo, String nickname, String favorTitle,
+			Date favorDate) {
+		super();
+		this.recipeNo = recipeNo;
+		this.nickname = nickname;
+		this.favorTitle = favorTitle;
+		this.favorDate = favorDate;
 	}
 
-	public void setRecipe(Recipe recipe) {
-		this.recipe = recipe;
+	public Integer getRecipeNo() {
+		return recipeNo;
 	}
 
-	public String getFavoriteTitle() {
-		return favoriteTitle;
+	public void setRecipeNo(Integer recipeNo) {
+		this.recipeNo = recipeNo;
 	}
 
-	public void setFavoriteTitle(String favoriteTitle) {
-		this.favoriteTitle = favoriteTitle;
+	public String getNickname() {
+		return nickname;
 	}
 
-	public Date getFavoriteDate() {
-		return favoriteDate;
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
 	}
 
-	public void setFavoriteDate(Date favoriteDate) {
-		this.favoriteDate = favoriteDate;
+	public String getFavorTitle() {
+		return favorTitle;
+	}
+
+	public void setFavorTitle(String favorTitle) {
+		this.favorTitle = favorTitle;
+	}
+
+	public Date getFavorDate() {
+		return favorDate;
+	}
+
+	public void setFavorDate(Date favorDate) {
+		this.favorDate = favorDate;
 	}
 
 	@Override
 	public String toString() {
-		return "Favorite [recipe=" + recipe + ", favoriteTitle="
-				+ favoriteTitle + ", favoriteDate=" + favoriteDate
-				+ ", toString()=" + super.toString() + "]";
+		return "Favorite [recipeNo=" + recipeNo + ", nickname=" + nickname
+				+ ", favorTitle=" + favorTitle + ", favorDate=" + favorDate
+				+ "]";
 	}
+
+	
 
 }
